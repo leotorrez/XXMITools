8a57bba284b2a31208791bb319614667172ebb62
leotorrez <leotorrez@gmail.com>
2024-08-13 21:51:45 -0300

    version upgrade to 1.2

--- __init__.py
+++ __init__.py
@@ -29,7 +29,7 @@ bl_info = {
     "description": "Imports meshes dumped with 3DMigoto's frame analysis and exports meshes suitable for re-injection.",
     "category": "Import-Export",
     "tracker_url": "https://github.com/leotorrez/XXMI-Tools",
-    "version" : (0, 0, 1),
+    "version" : (1, 2, 0),
 }
 auto_load.init()
 
