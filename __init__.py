e64af4e897e0ba91bcb9ae752f4c8140fccfd671
Leo <leotorrez@gmail.com>
2025-04-30 10:12:53 -0300

    Auto update segmented in it's own folder and credit rework

--- __init__.py
+++ __init__.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
-from . import auto_load
+from .autoupdate import auto_load
 
 # Original plugin by DarkStarSword (https://github.com/DarkStarSword/3d-fixes/blob/master/blender_3dmigoto.py)
 # Updated to support 3.0 by MicroKnightmare from the DOA modding discord
 
 ####### AGMG Discord Contributors #######
-
 # Modified by SilentNightSound#7430 to add Genshin support and some more Genshin-specific features
 # QOL feature (ignoring hidden meshes while exporting) added by HazrateGolabi#1364
 # HummyR#8131
 # merged several iterations of this plugin for other games back into a single one by LeoTorreZ
+# Testing and deveping of modern featres by SinsOfSeven
+# Added support for WUWA and more formal implementation of its classes by SpectrumQT
 
 bl_info = {
     "name": "XXMI_Tools",
     "blender": (2, 93, 0),
-    "author": "Ian Munsie (darkstarsword@gmail.com), SilentNightSound#7430, LeoTorreZ",
+    "author": "LeoTorreZ",
     "location": "File > Import-Export",
-    "description": "Imports meshes dumped with 3DMigoto's frame analysis and exports meshes suitable for re-injection.",
+    "description": "Imports meshes dumped with 3DMigoto's frame analysis and exports meshes suitable for re-injection. Author of original plugin: DarkStarSword. Contributors: SilentNightSound#7430, HazrateGolabi#1364, HummyR#8131, SinsOfSeven, SpectrumQT",
     "category": "Import-Export",
     "tracker_url": "https://github.com/leotorrez/XXMITools",
     "version" : (1, 4, 4),
