9def626656b76fa45e9f9b0dc4d6381bf554b1f7
Leo <leotorrez@gmail.com>
2026-02-18 08:49:20 -0300

    ADD: Support for missing texture files and reuse of already imported image files

--- migoto/data/hash_json.py
+++ migoto/data/hash_json.py
@@ -6,8 +6,10 @@ from pathlib import Path
 @dataclass
 class TextureData:
     name: str
+    fullname: str
     extension: str
     hash: str
+    path: Path
 
 
 @dataclass
@@ -39,9 +41,34 @@ class Component:
 class HashJsonData:
     def __init__(self, path: Path):
         name = path.parent.stem
+        self.path: Path = path
         with open(path, "r") as f:
             data = json.load(f)
         self.components = self.parse_components(data, name)
+        self.find_missing_textures()
+
+    def find_missing_textures(self):
+        texture_map: dict[str, Path] = {}
+        missing_textures: list[TextureData] = []
+        for comp in self.components:
+            for part in comp.parts:
+                for tex in part.textures:
+                    if tex.path.exists():
+                        texture_map[tex.hash] = tex.path
+                    else:
+                        missing_textures.append(tex)
+        if missing_textures:
+            print("Missing texture files:")
+            for tex in missing_textures:
+                if tex.hash in texture_map:
+                    tex.path = texture_map[tex.hash]
+                    print(
+                        f"{tex.name}{tex.extension} (hash: {tex.hash}) - found at {tex.path}"
+                    )
+                else:
+                    print(
+                        f"{tex.name}{tex.extension} (hash: {tex.hash}) at {tex.path} - file not found"
+                    )
 
     def parse_components(self, data: list[dict], name: str) -> list[Component]:
         comps: list[Component] = []
@@ -68,22 +95,33 @@ class HashJsonData:
             comp["object_indexes"],
             comp["object_classifications"],
         ):
-            textures = [
-                TextureData(
-                    name=tex_type,
-                    extension=tex_ext,
-                    hash=tex_hash,
-                )
-                for tex_type, tex_ext, tex_hash in tex_hashes
-            ]
+            part_fullname = name + comp["component_name"] + obj_class
+            textures = self.parse_textures(tex_hashes, part_fullname)
             parts.append(
                 Part(
-                    fullname=name + comp["component_name"] + obj_class,
+                    fullname=part_fullname,
                     textures=textures,
                     first_index=obj_index,
                 )
             )
         return parts
+
+    def parse_textures(
+        self, tex_hashes: list[list], part_fullname: str
+    ) -> list[TextureData]:
+        textures = []
+        for name, ext, hash in tex_hashes:
+            path = self.path.parent / (part_fullname + name + ext)
+            textures.append(
+                TextureData(
+                    name=name,
+                    fullname=part_fullname + name,
+                    extension=ext,
+                    hash=hash,
+                    path=path,
+                )
+            )
+        return textures
 
     def get_part_by_fullname(self, fullname: str) -> Part:
         for comp in self.components:
