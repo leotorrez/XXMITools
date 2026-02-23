4bb70137db5dd45b821706d2642c17c674a61fc1
Leo <leotorrez@gmail.com>
2026-02-23 18:34:21 -0300

    CHORE: importer.py cleanup

--- migoto/importer.py
+++ migoto/importer.py
@@ -3,11 +3,12 @@ from dataclasses import dataclass, field
 from pathlib import Path
 
 import bpy
+from bpy.types import Collection, Context, Mesh, Object, Operator
 from bpy_extras.io_utils import axis_conversion
 
 from .data.byte_buffer import AbstractSemantic, MigotoFormat
 from .data.data_model import DataModelXXMI
-from .data.hash_json import HashJsonData
+from .data.hash_json import Component, HashJsonData
 from .data.numpy_mesh import NumpyMesh, NumpyMeshGroup
 from .datahandling import Fatal
 from .datastructures import ImportPaths
@@ -59,11 +60,14 @@ def _extract_path(path_or_tuple: str | tuple | list[str]) -> Path:
 
 
 class ObjectImporter:
-    def import_object(self, operator, context, cfg):
+    def import_object(self, operator: Operator, context: Context, cfg):
         # Extract the first path, handling both string and tuple formats
         first_import_path = next(iter(cfg.import_paths))
         first_vb_path = _extract_path(first_import_path.vb_paths[0])
         import_folder: Path = Path(first_vb_path).parent
+
+        assert import_folder is not None
+        assert context.scene is not None and context.scene.collection is not None
 
         start_time = time.time()
         print(f"Object import started for '{import_folder.stem}' folder")
@@ -74,15 +78,16 @@ class ObjectImporter:
                 f"Specified folder is missing hash.json! Expected at: {import_folder / 'hash.json'}",
             )
 
-        imported_objects = self.process_objects(operator, cfg, hash_json_data)
+        imported_objects: list[Object] = self.process_objects(
+            operator, cfg, hash_json_data
+        )
         if len(imported_objects) == 0:
             raise Fatal(
                 "Specified folder is missing files for components!",
             )
 
-        assert import_folder is not None
+        col: Collection = bpy.data.collections.new(import_folder.stem)
 
-        col = bpy.data.collections.new(import_folder.stem)
         context.scene.collection.children.link(col)
         for obj in imported_objects:
             col.objects.link(obj)
@@ -92,14 +97,14 @@ class ObjectImporter:
 
         print(f"Total import time: {time.time() - start_time:.3f}s")
 
-    def process_objects(self, operator, cfg, hash_json_data) -> list[bpy.types.Object]:
-        imported_objects = []
+    def process_objects(self, operator: Operator, cfg, hash_json_data) -> list[Object]:
+        imported_objects: list[Object] = []
 
         # We use lists to ensure the order is kept
         grouped_paths: dict[str, list[ImportPaths]] = {}
         if cfg.merge_meshes:
             for component in hash_json_data.components:
-                fullname = component.fullname
+                fullname: str = component.fullname
                 result: list[ImportPaths] = []
                 for part in component.parts:
                     part_result = next(
@@ -118,13 +123,13 @@ class ObjectImporter:
         else:
             for paths in cfg.import_paths:
                 ib_path = Path(_extract_path(paths.ib_paths))
-                fullname = ib_path.stem.split("-ib")[0]
+                fullname: str = ib_path.stem.split("-ib")[0]
                 if fullname not in grouped_paths:
                     grouped_paths[fullname] = []
                 grouped_paths[fullname].append(paths)
 
         for fullname, paths in grouped_paths.items():
-            obj = self.import_component(
+            obj: Object = self.import_component(
                 operator,
                 cfg,
                 paths,
@@ -140,7 +145,7 @@ class ObjectImporter:
 
     def import_component(
         self,
-        operator,
+        operator: Operator,
         cfg: ImporterOptions,
         paths: list[ImportPaths],
         hash_json_data: HashJsonData,
@@ -170,9 +175,7 @@ class ObjectImporter:
                 )
             )
         if (format := numpy_mesh_group.numpy_mesh.format) is None:
-            raise Fatal(
-                f"Specified .fmt file for {fmt_path.stem} is missing vertex buffer layout!",
-            )
+            raise Fatal(f"Failed to determine vertex format for component {name}!")
         vg_remap = None
         # if cfg.import_skeleton_type == "MERGED":
         #     component_pattern = re.compile(r".*component[ -_]*([0-9]+).*")
@@ -181,15 +184,11 @@ class ObjectImporter:
         #         component = extracted_object.components[int(result[0])]
         #         vg_remap = numpy.array(list(component.vg_map.values()))
 
-        mesh = bpy.data.meshes.new(name)
-        obj = bpy.data.objects.new(mesh.name, mesh)
+        mesh: Mesh = bpy.data.meshes.new(name)
+        obj: Object = bpy.data.objects.new(mesh.name, mesh)
         assert mesh is not None and obj is not None
 
-        global_matrix = axis_conversion(
-            from_forward=axis_forward, from_up=axis_up
-        ).to_4x4()
-        obj.matrix_world = global_matrix
-
+        obj.matrix_world = axis_conversion(axis_forward, axis_up).to_4x4()
         model = DataModelXXMI()
         model.flip_winding = cfg.flip_winding
         model.flip_texcoord_v = cfg.flip_texcoord_v
@@ -206,7 +205,7 @@ class ObjectImporter:
             mirror_mesh=cfg.flip_mesh,
         )
 
-        num_shapekeys = (
+        num_shapekeys: int = (
             0
             if mesh.shape_keys is None
             else len(getattr(mesh.shape_keys, "key_blocks", []))
@@ -216,12 +215,15 @@ class ObjectImporter:
         )
         return obj
 
-    def cleanup_object(self, operator, context, obj, cfg):
+    def cleanup_object(
+        self, operator: Operator, context: Context, obj: Object, cfg: ImporterOptions
+    ):
         if cfg.merge_meshes and not cfg.clean_loose:
             operator.report(
                 {"WARNING"},
                 "Mesh merging enabled without loose geometry cleanup! This may result in floating vertex hidden among your mesh. Consider enabling 'Clean Loose' option to remove them.",
             )
+        assert context.view_layer is not None
         obj.select_set(True)
         context.view_layer.objects.active = obj
 
@@ -250,7 +252,7 @@ class ObjectImporter:
         #     context.view_layer.objects.active = obj
 
     def set_custom_properties(
-        self, obj, migoto_format: MigotoFormat, cfg: ImporterOptions
+        self, obj: Object, migoto_format: MigotoFormat, cfg: ImporterOptions
     ):
         if migoto_format.vb_layout is None or migoto_format.format is None:
             raise Fatal(
@@ -270,8 +272,8 @@ class ObjectImporter:
 
     def set_materials(
         self,
-        operator,
-        obj: bpy.types.Object,
+        operator: Operator,
+        obj: Object,
         name: str,
         cfg: ImporterOptions,
         hash_json_data: HashJsonData,
@@ -319,10 +321,9 @@ class ObjectImporter:
             obj.id_properties_ui(prop_name).update(id_type="MATERIAL", description="")
 
         if cfg.merge_meshes:
-            component = hash_json_data.get_component_by_fullname(name)
+            component: Component = hash_json_data.get_component_by_fullname(name)
             for part in component.parts:
-                diffuse = part.get_texture_by_name("diffuse")
-                if diffuse is None:
+                if (diffuse := part.get_texture_by_name("diffuse")) is None:
                     operator.report(
                         {"WARNING"},
                         f"Failed to find diffuse texture for {part.fullname} in dump folder!",
@@ -337,15 +338,13 @@ class ObjectImporter:
                 add_material(obj, part, diffuse)
             return
 
-        part = hash_json_data.get_part_by_fullname(name)
-        if part is None:
+        if (part := hash_json_data.get_part_by_fullname(name)) is None:
             operator.report(
                 {"WARNING"},
                 f"Failed to find diffuse texture for {name} in dump folder!",
             )
             return
-        diffuse = part.get_texture_by_name("diffuse")
-        if diffuse is None:
+        if (diffuse := part.get_texture_by_name("diffuse")) is None:
             operator.report(
                 {"WARNING"},
                 f"Failed to find diffuse texture for {name} in dump folder!!",
